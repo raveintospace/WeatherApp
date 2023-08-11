cc378f1689f23512080c8908bfbd63f0e14072f1
Uri <uriaeri@gmail.com>
2023-08-11 16:44:25 +0200

    search bar at top of view

--- WeatherAsync/View/ContentView.swift
+++ WeatherAsync/View/ContentView.swift
@@ -13,88 +13,86 @@ struct ContentView: View {
     @State private var location = ""
     
     var body: some View {
-        ZStack {
-            VStack {
-                Text(viewModel.weatherModelForView.city)
-                    .foregroundColor(.white)
-                    .font(.system(size: 70))
-                Text(viewModel.weatherModelForView.description)
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .padding(.bottom, 8)
-                HStack {
-                    if let url = viewModel.weatherModelForView.iconURL {
-                        AsyncImage(url: url) { image in
-                            image
-                        } placeholder: {
-                            ProgressView()
-                        }
-
-                    }
-                    Text(viewModel.weatherModelForView.currentTemperature)
-                        .font(.system(size: 70))
-                        .foregroundColor(.white)
-                }
-                .padding(.top, -20)
-                HStack(spacing: 14) {
-                    Label(viewModel.weatherModelForView.minTemperature, systemImage: "thermometer.snowflake")
-                    Label(viewModel.weatherModelForView.maxTemperature, systemImage: "thermometer.sun.fill")
-                }
-                .modifier(CustomModifier())
-                
-                Divider()
-                    .foregroundColor(.white)
-                    .padding()
-                
-                HStack(spacing: 32) {
-                    VStack {
-                        Image(systemName: "sunrise.fill")
-                            .symbolRenderingMode(.multicolor)
-                        Text(viewModel.weatherModelForView.sunrise, style: .time)
-                    }
-                    
-                    VStack {
-                        Image(systemName: "sunset.fill")
-                            .symbolRenderingMode(.multicolor)
-                        Text(viewModel.weatherModelForView.sunset, style: .time)
-                    }
-                }
-                .foregroundColor(.white)
-                
-                Divider()
-                    .foregroundColor(.white)
-                    .padding()
+        NavigationStack {
+            ZStack {
                 VStack {
-                    Label(viewModel.weatherModelForView.humidity, systemImage: "humidity.fill")
-                        .modifier(CustomModifier())
+                    Text(viewModel.weatherModelForView.city)
+                        .foregroundColor(.white)
+                        .font(.system(size: 70))
+                    Text(viewModel.weatherModelForView.description)
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding(.bottom, 8)
+                    HStack {
+                        if let url = viewModel.weatherModelForView.iconURL {
+                            AsyncImage(url: url) { image in
+                                image
+                            } placeholder: {
+                                ProgressView()
+                            }
+                            
+                        }
+                        Text(viewModel.weatherModelForView.currentTemperature)
+                            .font(.system(size: 70))
+                            .foregroundColor(.white)
+                    }
+                    .padding(.top, -20)
+                    HStack(spacing: 14) {
+                        Label(viewModel.weatherModelForView.minTemperature, systemImage: "thermometer.snowflake")
+                        Label(viewModel.weatherModelForView.maxTemperature, systemImage: "thermometer.sun.fill")
+                    }
+                    .modifier(CustomModifier())
                     
                     Divider()
                         .foregroundColor(.white)
                         .padding()
                     
-                    TextField("Enter location", text: $location)
-                        .frame(height: 100)
-                        .background(Color.white)
-                        .padding(.horizontal, 50)
-                        .onSubmit {
-                            Task {
-                                await viewModel.getWeather(city: location)
-                            }
+                    HStack(spacing: 32) {
+                        VStack {
+                            Image(systemName: "sunrise.fill")
+                                .symbolRenderingMode(.multicolor)
+                            Text(viewModel.weatherModelForView.sunrise, style: .time)
                         }
-                    Text("City: \(location)")
+                        
+                        VStack {
+                            Image(systemName: "sunset.fill")
+                                .symbolRenderingMode(.multicolor)
+                            Text(viewModel.weatherModelForView.sunset, style: .time)
+                        }
+                    }
+                    .foregroundColor(.white)
                     
-                    Spacer()
+                    Divider()
+                        .foregroundColor(.white)
+                        .padding()
+                    VStack {
+                        Label(viewModel.weatherModelForView.humidity, systemImage: "humidity.fill")
+                            .modifier(CustomModifier())
+                        
+                        Divider()
+                            .foregroundColor(.white)
+                            .padding()
+                        
+                        Spacer()
+                    }
                 }
-                
+                .padding(.top, 20)
             }
-            .padding(.top, 20)
+            .background(LinearGradient(colors: [.blue, .purple], startPoint: .topLeading, endPoint: .bottomTrailing)
+            )
+            .task {
+                await viewModel.getWeather(city: "Palafrugell")
+            }
         }
-        .background(LinearGradient(colors: [.blue, .purple], startPoint: .topLeading, endPoint: .bottomTrailing)
-        )
-        .task {
-            await viewModel.getWeather(city: "Palafrugell")
+        .searchable(text: $location, placement: .navigationBarDrawer(displayMode: .automatic), prompt: Text("Enter location"))
+        .autocorrectionDisabled()
+        .onSubmit(of: .search, getWeather)
+    }
+    
+    func getWeather() {
+        Task {
+            await viewModel.getWeather(city: location)
         }
-        
     }
 }
 
